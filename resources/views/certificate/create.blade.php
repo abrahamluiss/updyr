1bb5c702671d393491ba34a1f1004ac3e4c95f9a
abrahamluis <abraham105@hotmail.com>
2022-05-06 13:01:10 -0500

    create certificate front

---
+++ resources/views/certificate/create.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.app', ['activePage' => 'certificate', 'titlePage' => __('Certificados')])
+@push('css')
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css" />
+@endpush
+@push('buttons')
+    <a class="btn btn-primary mr-4" href="{{ route('certificate.create') }}">
+        <span>Atras</span>
+        <div class="ripple-container"></div>
+    </a>
+@endpush
+@section('content')
+    <div class="content">
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-header card-header-primary">
+                    <h4 class="card-title">INGRESA LOS DATOS</h4>
+                    <p class="card-category">Algunos campos seran obligatorios</p>
+                </div>
+                <div class="card-body">
+                    <div id="typography">
+                        <div class="card-title">
+                            <h2>Generador de texto</h2>
+                        </div>
+                        <div class="row m-6 col-12">
+                            <form action="{{ route('certificate.store') }}" method="POST">
+                                @csrf
+                                @method('POST')
+
+                                <div class="mb-3 row ">
+                                    <label for="title" class="col-sm-2 col-form-label">Titulo</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="title" aria-describedby="title"
+                                            name="title" autocomplete="off" placeholder="Titulo"
+                                            class="form-control-plaintext">
+                                    </div>
+                                </div>
+                                <div class="mb-3 row ">
+                                    <label for="author" class="col-sm-2 col-form-label">Autor</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="author" aria-describedby="author"
+                                            name="author" autocomplete="off" placeholder="Autor"
+                                            class="form-control-plaintext">
+                                    </div>
+                                </div>
+                                <div class="mb-3 row ">
+                                    <label for="Adviser" class="col-sm-2 col-form-label">Asesor</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="Adviser" aria-describedby="Adviser"
+                                            name="Adviser" autocomplete="off" placeholder="Asesor"
+                                            class="form-control-plaintext">
+                                    </div>
+                                </div>
+                                <div class="mb-3 row ">
+                                    <label for="Adviser" class="col-sm-2 col-form-label">Programa</label>
+                                    <div class="col-sm-10">
+
+                                        <select class="form-select" aria-label="Default select example">
+                                            <option value="Bachiller">Bachiller</option>
+                                            <option value="Titulo">Titulo</option>
+                                            <option value="Segunda Especialidad">Segunda Especialidad</option>
+                                            <option value="Complementación Academica">Complementación Academica</option>
+                                            <option value="Maestria">Maestria</option>
+                                            <option value="Doctorado">Doctorado</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="mb-3 row ">
+                                    <label for="faculty" class="col-sm-2 col-form-label">Facultad</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="faculty" aria-describedby="faculty"
+                                            name="faculty" autocomplete="off" placeholder="Facultad"
+                                            class="form-control-plaintext">
+                                    </div>
+                                </div>
+                                <div class="mb-3 row ">
+                                    <label for="originality" class="col-sm-2 col-form-label">Originalidad</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="originality" aria-describedby="originality"
+                                            name="originality" autocomplete="off" placeholder="Originalidad"
+                                            class="form-control-plaintext">
+                                    </div>
+                                </div>
+                                <div class="mb-3 row ">
+                                    <label for="similitude" class="col-sm-2 col-form-label">Similitud</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="similitude" aria-describedby="similitude"
+                                            name="similitude" autocomplete="off" placeholder="Similitud"
+                                            class="form-control-plaintext">
+                                    </div>
+                                </div>
+                                <div class="mb-3 row ">
+                                    <label for="date" class="col-sm-2 col-form-label">Fecha</label>
+                                    <div class="col-sm-10">
+                                        <input type="date" class="form-control" id="date" aria-describedby="date"
+                                            name="date" autocomplete="off" placeholder="Similitud"
+                                            class="form-control-plaintext">
+                                    </div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="observation">Observación</label>
+                                    <textarea class="form-control" name="observation" type="text" id="observation"></textarea>
+                                </div>
+                                <br>
+                                <button type="submit" class="btn btn-primary">Enviar</button>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@push('js')
+    <script>
+    </script>
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -18,41 +18,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 Auth::routes();
 
 Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home')->middleware('auth');
 
-Route::group(['middleware' => 'auth'], function () {
-	Route::get('table-list', function () {
-		return view('pages.table_list');
-	})->name('table');
-
-	Route::get('typography', function () {
-		return view('pages.typography');
-	})->name('typography');
-
-	Route::get('icons', function () {
-		return view('pages.icons');
-	})->name('icons');
-
-	Route::get('map', function () {
-		return view('pages.map');
-	})->name('map');
-
-	Route::get('notifications', function () {
-		return view('pages.notifications');
-	})->name('notifications');
-
-	Route::get('rtl-support', function () {
-		return view('pages.language');
-	})->name('language');
-
-	Route::get('upgrade', function () {
-		return view('pages.upgrade');
-	})->name('upgrade');
-});
 
 Route::group(['middleware' => 'auth'], function () {
 	Route::resource('user', 'App\Http\Controllers\UserController', ['except' => ['show']]);
@@ -65,8 +36,6 @@ Route::group(['middleware' => 'auth'], function () {
 });
 
 Auth::routes();
-
-Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home')->middleware('auth');
 
 
 Route::group(['middleware' => 'auth'], function () {
